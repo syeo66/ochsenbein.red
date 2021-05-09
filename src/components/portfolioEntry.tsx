a3df7ed342a581bf61be488d8702c7a514c8f5dd
Red Ochsenbein <red@redochsenbein.ch>
2021-05-09 19:47:28 +0200

    Improve portfolio shadow

--- src/components/portfolioEntry.tsx
+++ src/components/portfolioEntry.tsx
@@ -27,8 +27,7 @@ const EntryBody = styled.div``
 const EntryImage = styled(GatsbyImage)`
   border-radius: 0.25rem;
   border: solid 1px #ddd;
-  box-shadow: 0 23.1px 31px rgba(0, 0, 0, 0.038), 0 28px 56.1px rgba(0, 0, 0, 0.065),
-    0 29.5px 79.8px rgba(0, 0, 0, 0.091), 0 31.6px 113.3px rgba(0, 0, 0, 0.128), 0 46px 225px rgba(0, 0, 0, 0.26);
+  box-shadow: 0 0 45px rgba(120, 10, 10, 0.4);
 `
 
 interface PortfolioEntryProps {
