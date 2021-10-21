a14d23f759f6f213991016aa6eebeb476cbaa0f1
Red Ochsenbein <red@redochsenbein.ch>
2021-10-21 10:27:57 +0200

    Add svelte to introduction text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,9 @@ const IndexPage = () => (
     <h2>Hi people</h2>
     <p>
       Yeah, this is really my name. I'm a Software Developer with many years of experience. At the moment I am diving
-      deep into Typescript, React and Elixir. But I have experience in a lot of other languages and technologies like
-      Go, Dart, Python and PHP. I already did a lot in my live, from being a banker, playing music on tour, jumping out
-      of planes, being a music composer, to being a freelance web developer, and designing t-shirts.
+      deep into Typescript, React, Svelte and Elixir. But I have experience in a lot of other languages and technologies
+      like Go, Dart, Python and PHP. I already did a lot in my live, from being a banker, playing music on tour, jumping
+      out of planes, being a music composer, to being a freelance web developer, and designing t-shirts.
     </p>
     <ul>
       <li>🌱 Vegan</li>
